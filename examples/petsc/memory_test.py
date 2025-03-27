3613d4b12eccb48c1f4afc5dd301cf23b40f6d69
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2025-03-27 15:30:32 +0000

    add example

---
+++ examples/petsc/memory_test.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+
+from devito import (Grid, Function, Eq, Operator, configuration,
+                    switchconfig)
+from devito.types import Symbol
+from devito.types.equation import PetscEq
+from devito.data.allocators import ALLOC_PETSC
+from devito.petsc import PETScSolve
+from devito.petsc.types import AllocateMemory
+from devito.petsc.initialize import PetscInitialize
+configuration['compiler'] = 'custom'
+os.environ['CC'] = 'mpicc'
+
+PetscInitialize()
+
+nx = 81
+ny = 81
+
+grid = Grid(shape=(nx, ny), extent=(2., 2.), dtype=np.float64)
+
+u = Function(name='u', grid=grid, dtype=np.float64, space_order=2, allocator=ALLOC_PETSC)
+v = Function(name='v', grid=grid, dtype=np.float64, space_order=2, allocator=ALLOC_PETSC)
+
+v.data[:] = 5.0
+
+eq = Eq(v, u.laplace, subdomain=grid.interior)
+
+petsc = PETScSolve([eq], u)
+
+
+with switchconfig(language='petsc'):
+    op = Operator(petsc)
+
+
+# print(op.ccode)
+op.apply()
+
+
+
